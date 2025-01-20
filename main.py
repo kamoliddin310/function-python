9478f3ea25c40c53429ccc73028bfdbaf306dd9b
djumanov <djumanovdev@gmail.com>
2025-01-20 17:27:45 +0500

    task

--- main.py
+++ main.py
@@ -1,18 +1,62 @@
-users = [
-    {"name": "Alice", "age": 25, "job": "Engineer"},
-    {"name": "Bob", "age": 30, "job": "Designer"},
-    {"name": "Charlie", "age": 22, "job": "Developer"},
-    {"name": "Diana", "age": 28, "job": "Teacher"},
-    {"name": "Edward", "age": 35, "job": "Manager"},
-    {"name": "Fiona", "age": 27, "job": "Data Analyst"},
-    {"name": "George", "age": 29, "job": "Marketing Specialist"},
-    {"name": "Hannah", "age": 26, "job": "Researcher"},
-    {"name": "Ian", "age": 31, "job": "Accountant"},
-    {"name": "Julia", "age": 24, "job": "HR Specialist"}
-]
+def menu() -> None:
+    print("\nMenu:")
+    print("\t1. Register User\n\t2. Filter users by age\n\t3. Filter users by job.\n\t4. Get all users")
 
-adults = filter(lambda user: user['age'] > 27, users)
+def print_users(users: list[dict]) -> list[dict]:
+    print("\nA List of Users:")
+    for number, user in enumerate(users, start=1):
+        print(f"{number}. {user['name']} is {user['age']} and {user['job']}.")
 
-for a in adults:
-    print(a)
+def add_user(users: list[dict]) -> list[dict]:
+    print("Enter data for new user")
+    name = input("Name: ")
+    age = int(input("Age: "))
+    job = input("Job: ")
+
+    new_user = {
+        "name": name,
+        "age": age,
+        "job": job,
+    }
+    users.append(new_user)
+    return users
+
+def get_users_by_age(users: list[dict], age: int) -> list[dict]:
+    pass
+
+def get_users_by_job(users: list[dict], job: str) -> list[dict]:
+    pass
+
+def main():
+    users = [
+        {"name": "Alice", "age": 25, "job": "Engineer"},
+        {"name": "Bob", "age": 30, "job": "Designer"},
+        {"name": "Charlie", "age": 22, "job": "Developer"},
+        {"name": "Diana", "age": 28, "job": "Teacher"},
+        {"name": "Edward", "age": 35, "job": "Manager"},
+        {"name": "Fiona", "age": 27, "job": "Data Analyst"},
+        {"name": "George", "age": 29, "job": "Marketing Specialist"},
+        {"name": "Hannah", "age": 26, "job": "Researcher"},
+        {"name": "Ian", "age": 31, "job": "Accountant"},
+        {"name": "Julia", "age": 24, "job": "HR Specialist"}
+    ]
+
+    while True:
+        menu()
+
+        choice = input("Enter Option: ")
+        if choice == '1':
+            users = add_user(users)
+        elif choice == '2':
+            age = int(input("Enter min age: "))
+            filtered_users = get_users_by_age(users, age)
+        elif choice == '3':
+            job = int(input("Enter job: "))
+            filtered_users = get_users_by_job(users, job)
+        elif choice == '4':
+            print_users(users)
+        else:
+            print("Invalid choice.")
+
+main()
 
--- mapping.py
+++ mapping.py
@@ -1,6 +1,15 @@
+users = [
+    {"name": "Alice", "age": 25, "job": "Engineer"},
+    {"name": "Bob", "age": 30, "job": "Designer"},
+    {"name": "Charlie", "age": 22, "job": "Developer"},
+    {"name": "Diana", "age": 28, "job": "Teacher"},
+    {"name": "Edward", "age": 35, "job": "Manager"},
+    {"name": "Fiona", "age": 27, "job": "Data Analyst"},
+    {"name": "George", "age": 29, "job": "Marketing Specialist"},
+    {"name": "Hannah", "age": 26, "job": "Researcher"},
+    {"name": "Ian", "age": 31, "job": "Accountant"},
+    {"name": "Julia", "age": 24, "job": "HR Specialist"}
+]
 
-names = ["ali", "vAli", "joHn", "DOE"]
-
-names = map(lambda name: name.title(), names)
-
-print(list(names))
+ages = map(lambda user: user['age'], users)
+print(list(ages))
