66d75396aee8afb3687bfd7e128da9cf5e8020d9
djumanov <djumanovdev@gmail.com>
2025-01-20 16:48:49 +0500

    filter

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+users = [
+    {"name": "Alice", "age": 25, "job": "Engineer"},
+    {"name": "Bob", "age": 30, "job": "Designer"},
+    {"name": "Charlie", "age": 22, "job": "Developer"},
+    {"name": "Diana", "age": 28, "job": "Teacher"},
+    {"name": "Edward", "age": 35, "job": "Manager"},
+    {"name": "Fiona", "age": 27, "job": "Data Analyst"},
+    {"name": "George", "age": 29, "job": "Marketing Specialist"},
+    {"name": "Hannah", "age": 26, "job": "Researcher"},
+    {"name": "Ian", "age": 31, "job": "Accountant"},
+    {"name": "Julia", "age": 24, "job": "HR Specialist"}
+]
 
+def lambda_func(user):
+    return user['age'] > 27 and user['age'] < 30
 
-def sum_of(n: int) -> int:
-    if n == 1:
-        return 1
-
-    return n + sum_of(n - 1)
-
-s = sum_of(10)
-print(s)
+# adults = filter(lambda user: user['age'] > 27 and user['age'] < 30, users)
+adults = filter(lambda_func, users)
+print(list(adults))
 
