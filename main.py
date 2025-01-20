3cfc3bd7e8a14a1501d858165edaeaeb831e378d
djumanov <djumanovdev@gmail.com>
2025-01-20 16:18:12 +0500

    recursive

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
 
-def get_max(num1: int, num2: int, num3: int) -> int:
-    """Bu function berilgan 3 ta sonning eng kattasini topib beradi.
 
-    Args:
-        - num1 (int): Birinchi son.
-        - num2 (int): ikkinchi son.
-        - num3 (int): uchinchi son.
+def sum_of(n: int) -> int:
+    if n == 1:
+        return 1
 
-    Returns:
-        int: Eng katta son.
-    """
-    mx = num1
-    if mx < num2:
-        mx = num2
-    if mx < num3:
-        mx = num3
-    
-    return mx
+    return n + sum_of(n - 1)
 
-m = get_max(-2, -7, -5)
-print(m)
+s = sum_of(10)
+print(s)
+
